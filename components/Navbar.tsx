85afe45dc90e5e0a912a9e9eac425386f55f5360
hasanrc24 <hasanrc24@gmail.com>
2022-12-14 13:09:51 +0600

    Nav

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,36 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import { FaBars } from "react-icons/fa";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav>
+      <Link href="/" className="d-flex align-items-center">
+        <Image src="/logo.png" alt="logo" height={40} width={40} />
+        <span className="logo-txt fw-bold">Next Blog</span>
+      </Link>
+      <FaBars className="bars" />
+      <ul className="nav-list">
+        <li className="links">
+          <Link href="/products">Products</Link>
+        </li>
+        <li className="links">
+          <Link href="/pricing">Pricing</Link>
+        </li>
+        <li className="links">
+          <Link href="/docs">Docs</Link>
+        </li>
+        <li className="links">
+          <Link href="/company">Company</Link>
+        </li>
+      </ul>
+      <div className="nav-btn">
+        <button className="btn">Log In</button>
+        <button className="btn btn-sign">Sign Up</button>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
