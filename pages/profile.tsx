feb9d10217b04fa2a75505d72201ba7dde5b7bd8
hasanrc24 <hasanrc24@gmail.com>
2022-12-25 13:12:57 +0600

    Profile

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -4,7 +4,120 @@ import { API_URL } from "../config/config";
 
 const Profile = ({ data }: any) => {
   console.log(data);
-  return <div className="">{data.username}</div>;
+
+  const { firstName, lastName, username, email } = data;
+  return (
+    <section style={{ backgroundColor: "" }}>
+      <div className="container py-5">
+        <div className="row">
+          <div className="col-lg-4">
+            <div className="card mb-4">
+              <div className="card-body text-center">
+                <img
+                  src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp"
+                  alt="avatar"
+                  className="rounded-circle img-fluid"
+                  style={{ width: "150px" }}
+                />
+                <h5 className="my-3">{firstName + " " + lastName}</h5>
+                <p className="text-muted mb-1">@{username}</p>
+                <p className="text-muted mb-4">Bay Area, San Francisco, CA</p>
+                <div className="d-flex justify-content-center mb-2"></div>
+              </div>
+            </div>
+            <div className="card mb-4 mb-lg-0">
+              <div className="card-body p-0">
+                <ul className="list-group list-group-flush rounded-3">
+                  <li className="list-group-item d-flex justify-content-between align-items-center p-3">
+                    <i className="fas fa-globe fa-lg text-warning"></i>
+                    <p className="mb-0">https://www.nextblog.com</p>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-8">
+            <div className="card mb-4">
+              <div className="card-body">
+                <div className="row">
+                  <div className="col-sm-3">
+                    <p className="mb-0">Full Name</p>
+                  </div>
+                  <div className="col-sm-9">
+                    <p className="text-muted mb-0">
+                      {firstName + " " + lastName}
+                    </p>
+                  </div>
+                </div>
+                <hr />
+                <div className="row">
+                  <div className="col-sm-3">
+                    <p className="mb-0">Email</p>
+                  </div>
+                  <div className="col-sm-9">
+                    <p className="text-muted mb-0">{email}</p>
+                  </div>
+                </div>
+                <hr />
+                <div className="row">
+                  <div className="col-sm-3">
+                    <p className="mb-0">Phone</p>
+                  </div>
+                  <div className="col-sm-9">
+                    <p className="text-muted mb-0"></p>
+                  </div>
+                </div>
+                <hr />
+                <div className="row">
+                  <div className="col-sm-3">
+                    <p className="mb-0">Mobile</p>
+                  </div>
+                  <div className="col-sm-9">
+                    <p className="text-muted mb-0"></p>
+                  </div>
+                </div>
+                <hr />
+                <div className="row">
+                  <div className="col-sm-3">
+                    <p className="mb-0">Address</p>
+                  </div>
+                  <div className="col-sm-9">
+                    <p className="text-muted mb-0"></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-6">
+                <div className="card mb-4 mb-md-0">
+                  <div className="card-body">
+                    <p className="mb-4">
+                      <span className="text-primary font-italic me-1">
+                        assigment
+                      </span>{" "}
+                      Project Status
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-6">
+                <div className="card mb-4 mb-md-0">
+                  <div className="card-body">
+                    <p className="mb-4">
+                      <span className="text-primary font-italic me-1">
+                        assigment
+                      </span>{" "}
+                      Project Status
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Profile;
